68da616c82fb3c7c25f6d6852e55ea7a4abac456
Ismael Mansoor <1ismaelmansoor@gmail.com>
2023-06-14 10:52:36 +0200

    our first test

---
+++ README.md
@@ -0,0 +1,6 @@
+#Our Git Lesson
+
+we are trying GIT for the first time.
+Let us hope it's not an addictive substance
+
+
---
+++ gittestcode.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+var greeting = "Hello, GIT"
+print(greeting)
+
+//should we add sum function
+
+func sum(_ num1: Int, _ num2: Int) -> Int
+{
+    return num1 + num2
+}
+
+print(sum(10, 15))
+
+print("we have staged and committed")
+
+func mult(_ num1: Int, _ num2: Int) -> Int{
+    return num1 * num2
+}
+print(mult(10,25))
+
